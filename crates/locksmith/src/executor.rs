7020129b90e9d963f8a267bddf4ae0ce74fc998a
Thomas Forbes <tom.forbes@gitguardian.com>
2025-02-08 19:35:34 +0000

    Add introspection and locker implementations, and initial tests

--- crates/locksmith/src/executor.rs
+++ crates/locksmith/src/executor.rs
@@ -1,17 +1,213 @@
 use crate::ConnectionID;
+use anyhow::{bail, Context};
+use std::pin::pin;
+use tokio_postgres::tls::NoTlsStream;
+use tokio_postgres::{AsyncMessage, Client, Connection, NoTls, Socket};
+use tracing::{debug, trace};
 
-pub struct StatementExecutor {}
+/// A [StatementExecutor] is a client for executing statements on a Postgres database.
+/// It provides a method, [StatementExecutor::check_statement_for_locks], which executes a statement
+/// and returns true if the statement was blocked by a lock.
+pub struct StatementExecutor {
+    client: Client,
+    connection: Connection<Socket, NoTlsStream>,
+    connection_id: ConnectionID,
+}
 
 impl StatementExecutor {
+    /// Create a new [StatementExecutor] with a connection to the Postgres database at `dsn`.
     pub async fn new(dsn: &str) -> anyhow::Result<Self> {
-        todo!()
+        // We have to use [tokio-postgres](https://crates.io/crates/tokio-postgres) for this, because
+        // sqlx does not give us the ability to receive NOTICE messages from the server.
+        let (client, mut connection) = tokio_postgres::connect(dsn, NoTls)
+            .await
+            .context("Creating connection")?;
+
+        // There are some peculiarities when using tokio-postgres compared to sqlx, namely that the
+        // client and the connection are separate and need to be driven separately.
+        // We do this by using `tokio::select!` to drive them both in parallel.
+        // If the connection future ever finishes before the client future then we bail out.
+        let connection_id = tokio::select! {
+            row = client.query_one("SELECT pg_backend_pid()", &[]) => {
+                let row = row.context("Query error while retrieving connection ID")?;
+                ConnectionID(row.get(0))
+            },
+            _ = &mut connection => {
+                bail!("Connection unexpectedly finished: retrieving connection ID")
+            }
+        };
+
+        // These statements are necessary to enable logging of lock waits. See
+        // `detect_if_statement_blocks` for the implementation details.
+        const SETUP_STATEMENTS: &str = r#"
+            BEGIN;
+            SET log_lock_waits=true;
+            SET deadlock_timeout='1ms';
+            SET client_min_messages='log';
+        "#;
+        tokio::select! {
+            setup_result = client.batch_execute(SETUP_STATEMENTS) => {
+                setup_result.context("Query error while executing setup statement")?
+            },
+            _ = &mut connection => bail!("Connection unexpectedly finished: executing setup statement")
+        }
+
+        Ok(Self {
+            client,
+            connection,
+            connection_id,
+        })
     }
 
+    /// Get the connection ID for this [StatementExecutor].
     pub fn connection_id(&self) -> ConnectionID {
-        todo!()
+        self.connection_id
     }
 
+    /// the statement was blocked by a lock.
+    ///
+    /// Postgres takes some locks when `COMMIT`ing a transaction, so this method will also attempt
+    /// to commit the transaction after executing the statement.
+    ///
+    /// If this method returns `true`, the connection and transaction will still be open and
+    /// blocked until the [StatementExecutor] is dropped. This allows the caller to inspect
+    /// the locks taken by the statement via another connection.
+    #[tracing::instrument(skip(self, statement))]
     pub async fn check_statement_for_locks(&mut self, statement: &str) -> anyhow::Result<bool> {
-        todo!()
+        for to_execute in [statement, "COMMIT;"] {
+            let is_blocked = self.detect_if_statement_blocks(to_execute).await?;
+            if is_blocked {
+                return Ok(true);
+            }
+        }
+        debug!("Statement succeeded");
+        Ok(false)
+    }
+
+    /// Detect if a statement is blocked by a lock. This
+    ///
+    /// The implementation of this method relies on a Postgres server feature called
+    /// [log_lock_waits](https://pgpedia.info/l/log_lock_waits.html).
+    /// In [StatementExecutor::new] we enable this option for our transaction, which causes the
+    /// server to send us a NOTICE message if the statement is blocked by a lock.
+    /// Server messages are delivered asynchronously to the [Connection], so we poll both the
+    /// [Connection] and the [Client] in parallel to drive the query and receive messages.
+    ///
+    /// If the server delivers us a NOTICE message that the statement is blocked, we stop polling
+    /// and return `true`. In this state the connection is still open and the transaction is still
+    /// intact.
+    ///
+    /// When the [StatementExecutor] is dropped the connection will be closed and the transaction
+    /// will be aborted.
+    #[tracing::instrument(skip(self, statement))]
+    async fn detect_if_statement_blocks(&mut self, statement: &str) -> anyhow::Result<bool> {
+        let mut poll_message_future = std::future::poll_fn(|cx| self.connection.poll_message(cx));
+        let mut execute_future = pin!(self.client.batch_execute(statement));
+
+        // Drive both the query future and the message future in parallel.
+        loop {
+            tokio::select! {
+                res = &mut execute_future => {
+                    res.context("Failed to execute statement")?;
+                    debug!("Statement executed successfully");
+                    return Ok(false)
+                },
+                async_message = &mut poll_message_future => {
+                    match async_message {
+                        None => {
+                            bail!("Connection unexpectedly finished: executing statement")
+                        }
+                        Some(msg) => {
+                            let async_message = msg.context("Reading message from server")?;
+                            trace!(?async_message, "Received message");
+
+                            if let AsyncMessage::Notice(msg) = async_message {
+                                // Postgres doesn't provide a structured code for notice messages,
+                                // so the best we can do is check the message text.
+                                let message = msg.message();
+                                if message.contains("still waiting for") {
+                                    debug!("Statement blocked");
+                                    return Ok(true)
+                                }
+                            }
+
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::executor::StatementExecutor;
+    use crate::tests::{lock_tables, start_test_postgres, table_exists};
+    use tracing_test::traced_test;
+
+    #[traced_test]
+    #[tokio::test]
+    async fn test_get_connection_id() {
+        let (_container, dsn) = start_test_postgres().await;
+        let executor = StatementExecutor::new(&dsn).await.unwrap();
+        assert!(executor.connection_id.0 > 0)
+    }
+    #[traced_test]
+    #[tokio::test]
+    async fn test_check_statement_does_not_commit_if_blocked() {
+        let (_container, dsn) = start_test_postgres().await;
+        let mut executor = StatementExecutor::new(&dsn).await.unwrap();
+        let _locker = lock_tables(&dsn, ["orders"]).await;
+        let is_blocked = executor
+            .check_statement_for_locks("drop table orders;")
+            .await
+            .unwrap();
+        assert!(is_blocked);
+        assert!(table_exists(&dsn, "orders").await);
+    }
+
+    #[traced_test]
+    #[tokio::test]
+    async fn test_check_statement_commits() {
+        let (_container, dsn) = start_test_postgres().await;
+        let mut executor = StatementExecutor::new(&dsn).await.unwrap();
+        let is_blocked = executor
+            .check_statement_for_locks("drop table orders;")
+            .await
+            .unwrap();
+        assert!(!is_blocked);
+        assert!(!table_exists(&dsn, "orders").await);
+    }
+
+    #[traced_test]
+    #[tokio::test]
+    async fn test_check_statement_with_invalid_sql() {
+        let (_container, dsn) = start_test_postgres().await;
+        let mut executor = StatementExecutor::new(&dsn).await.unwrap();
+        assert!(executor.check_statement_for_locks("foobar").await.is_err());
+    }
+
+    #[traced_test]
+    #[tokio::test]
+    async fn test_is_statement_blocked() {
+        let (_container, dsn) = start_test_postgres().await;
+
+        assert!(!StatementExecutor::new(&dsn)
+            .await
+            .unwrap()
+            .detect_if_statement_blocks("select * from customers")
+            .await
+            .unwrap());
+
+        let _locker = lock_tables(&dsn, ["customers"]).await;
+
+        assert!(StatementExecutor::new(&dsn)
+            .await
+            .unwrap()
+            .detect_if_statement_blocks("select * from customers")
+            .await
+            .unwrap());
+        drop(_locker)
     }
 }
--- crates/locksmith/src/introspection.rs
+++ crates/locksmith/src/introspection.rs
@@ -1,18 +1,194 @@
 use crate::objects::DBObject;
+use crate::{ColumnObject, IndexObject, TableObject};
+use anyhow::Context;
+use sqlx::{query, query_as, Connection, PgConnection};
 use std::collections::{HashMap, HashSet};
 
-pub struct Introspector {}
+/// An [Introspector] provides various introspection functions for a given Postgres database.
+/// Namely, it provides methods to list objects via [Introspector::list_objects], and to list
+/// file nodes for objects via [Introspector::list_object_file_nodes].
+pub struct Introspector {
+    conn: PgConnection,
+}
 
 impl Introspector {
+    /// Construct a new [Introspector] with a connection to the Postgres database at `dsn`.
     pub async fn new(dsn: &str) -> anyhow::Result<Self> {
-        todo!()
+        let mut conn = PgConnection::connect(dsn)
+            .await
+            .context("Creating connection")?;
+        conn.ping().await.context("Pinging postgres")?;
+        Ok(Self { conn })
     }
 
-    pub async fn list_object_file_nodes(&mut self) -> anyhow::Result<HashMap<DBObject, i32>> {
-        todo!()
-    }
-
+    /// ## List all objects in the database
+    /// This returns the set of all tables, columns and indexes in the database.
     pub async fn list_objects(&mut self) -> anyhow::Result<HashSet<DBObject>> {
-        todo!()
+        let tables = self.list_tables().await?.into_iter().map(DBObject::from);
+        let columns = self.list_columns().await?.into_iter().map(DBObject::from);
+        let indexes = self.list_indexes().await?.into_iter().map(DBObject::from);
+        Ok(tables.chain(columns).chain(indexes).collect())
+    }
+
+    /// List the file nodes of all objects in the database.
+    ///
+    /// A file node is a unique identifier for a table's underlying storage file, which
+    /// is guaranteed to change if the table is rewritten (even if the table is empty).
+    /// This uses the [pg_relation_filenode](https://pgpedia.info/p/pg_relation_filenode.html)
+    /// function to get the file node for each table.
+    ///
+    /// **Note**: Currently this only lists table file nodes, and not indexes or other objects.
+    pub async fn list_object_file_nodes(&mut self) -> anyhow::Result<HashMap<DBObject, i32>> {
+        query!(
+            r#"
+            SELECT table_name as "table!", pg_relation_filenode(table_name::text)::int as "file_node!"
+            FROM information_schema.tables
+            WHERE table_schema = "current_schema"()
+              AND table_catalog = current_database()
+            order by table_name;"#
+        )
+            .fetch_all(&mut self.conn)
+            .await.context("Query error while listing table file nodes")
+            .map(|r| {
+                r.into_iter().map(|r| {
+                    let table = DBObject::Table(TableObject {
+                        name: r.table,
+                    });
+                    (table, r.file_node)
+                }).collect()
+            })
+    }
+
+    /// ## List all tables in the database.
+    /// This uses the [information_schema.tables](https://www.postgresql.org/docs/current/infoschema-tables.html)
+    /// view to retrieve tables in the current schema.
+    pub async fn list_tables(&mut self) -> anyhow::Result<Vec<TableObject>> {
+        query_as!(
+            TableObject,
+            r#"
+            SELECT table_name as "name!"
+            FROM information_schema.tables
+            WHERE table_schema = "current_schema"()
+              AND table_catalog = current_database()
+            order by table_name;"#
+        )
+        .fetch_all(&mut self.conn)
+        .await
+        .context("Query error while listing tables")
+    }
+
+    /// ## List columns in the database
+    /// This uses the [information_schema.columns](https://www.postgresql.org/docs/current/infoschema-columns.html)
+    /// view to retrieve columns in the current schema.
+    pub async fn list_columns(&mut self) -> anyhow::Result<Vec<ColumnObject>> {
+        query_as!(
+            ColumnObject,
+            r#"
+            SELECT table_name as "table!", column_name as "name!", data_type as "data_type!"
+            FROM information_schema.columns
+            WHERE table_schema = "current_schema"()
+              AND table_catalog = current_database()
+            order by table_name, column_name;
+            "#
+        )
+        .fetch_all(&mut self.conn)
+        .await
+        .context("Query error while listing columns")
+    }
+
+    /// ## List indexes in the database
+    /// This uses the [pg_stat_all_indexes](https://pgpedia.info/p/pg_stat_all_indexes.html) view
+    /// to retrieve indexes in the current schema.
+    pub async fn list_indexes(&mut self) -> anyhow::Result<Vec<IndexObject>> {
+        query_as!(
+            IndexObject,
+            r#"
+            SELECT relname as "table!", indexrelname as "name!"
+            FROM pg_stat_all_indexes
+            WHERE schemaname = "current_schema"()
+            order by 1, 2;"#
+        )
+        .fetch_all(&mut self.conn)
+        .await
+        .context("Query error while listing indexes")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::start_test_postgres;
+
+    use tracing_test::traced_test;
+
+    #[traced_test]
+    #[tokio::test]
+    async fn test_list_tables() {
+        let (_container, dsn) = start_test_postgres().await;
+        let mut target = Introspector::new(&dsn).await.unwrap();
+        let tables = target.list_tables().await.unwrap();
+        assert_eq!(tables, vec!["customers".into(), "orders".into()]);
+    }
+    #[traced_test]
+    #[tokio::test]
+    async fn test_list_columns() {
+        let (_container, dsn) = start_test_postgres().await;
+        let mut target = Introspector::new(&dsn).await.unwrap();
+        let columns = target.list_columns().await.unwrap();
+        assert_eq!(
+            columns,
+            vec![
+                ColumnObject {
+                    table: "customers".into(),
+                    name: "id".to_string(),
+                    data_type: "integer".to_string()
+                },
+                ColumnObject {
+                    table: "customers".into(),
+                    name: "name".to_string(),
+                    data_type: "text".to_string()
+                },
+                ColumnObject {
+                    table: "orders".into(),
+                    name: "customer_id".to_string(),
+                    data_type: "integer".to_string()
+                },
+                ColumnObject {
+                    table: "orders".into(),
+                    name: "id".to_string(),
+                    data_type: "integer".to_string()
+                },
+                ColumnObject {
+                    table: "orders".into(),
+                    name: "price".to_string(),
+                    data_type: "numeric".to_string()
+                }
+            ]
+        );
+    }
+    #[traced_test]
+    #[tokio::test]
+    async fn test_list_indexes() {
+        let (_container, dsn) = start_test_postgres().await;
+        let mut target = Introspector::new(&dsn).await.unwrap();
+
+        let indexes = target.list_indexes().await.unwrap();
+        assert_eq!(
+            indexes,
+            vec![
+                IndexObject {
+                    table: "customers".into(),
+                    name: "customers_pkey".to_string()
+                },
+                IndexObject {
+                    table: "orders".into(),
+                    name: "orders_pkey".to_string()
+                },
+                IndexObject {
+                    table: "orders".into(),
+                    name: "orders_price_idx".to_string()
+                }
+            ]
+        )
     }
 }
